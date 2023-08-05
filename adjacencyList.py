ee2d7479b2f49b3ad1bd43c318ac3a4436e72ed8
andychen482 <andy.chen0607@gmail.com>
2023-08-04 22:20:30 -0400

    fully working

--- adjacencyList.py
+++ adjacencyList.py
@@ -120,5 +120,5 @@ def initiateList(course_graph, G, selected_major):
 
 if __name__ == '__main__':
     #For server use
-    #app.run(host='0.0.0.0', port=5000, debug=False)
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
+    # app.run(debug=True)
